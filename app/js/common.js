f76693007741238d753dc62ad7f8591f8f7de68d
Елена Портная <e.portnaya@largespace.ru>
2018-11-06 13:07:03 +0200

    slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    //tabs
+//tabs
     $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
         $(this)
           .addClass('active').siblings().removeClass('active')
@@ -42,9 +42,9 @@ function popups(elem, popup) {
         popup.removeClass('active');
         $('body').removeClass('active');
     })
-    $('.custom-click').click(function() {
-        $('.customform').removeClass('active');
-    })
+    // $('.custom-click').click(function() {
+    //     $('.customform').removeClass('active');
+    // })
     // popup.click(function(e) {
     //     e.preventDefault();
     //     e.stopPropagation();
@@ -52,6 +52,58 @@ function popups(elem, popup) {
 }
 
 popups($('.global-form'), $('.globalform'));
+
+//slider 
+if(jQuery('.reviews__slider').length) {
+  $('.reviews__slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    // prevArrow: '<i class="fa fa-angle-left feedback-slider-arrows arrow-left_mob"></i>',
+    prevArrow: '<svg class="svg-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"></path></svg>',
+    // nextArrow: '<i class="fa fa-angle-right feedback-slider-arrows arrow-right_mob"></i>',
+    nextArrow: '<svg class="svg-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M17.525 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L205.947 256 10.454 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L34.495 36.465c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg>',
+
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+}
+if(jQuery('.about-slider').length) {
+  $('.about-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+
+  });
+}
 
 
 
