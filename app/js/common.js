ea89020c87973e936a027c64d52fa2ca3b16dbc5
Елена Портная <e.portnaya@largespace.ru>
2018-12-02 10:48:26 +0200

    first

--- app/js/common.js
+++ app/js/common.js
@@ -1,15 +1,18 @@
 $(function() {
 
 //tabs
+if(jQuery('ul.tabs__caption').length) {
     $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
         $(this)
           .addClass('active').siblings().removeClass('active')
           .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
+    });
+}
 //tel
+if(jQuery('#tel').length) {
     var input = document.querySelector("#tel");
     window.intlTelInput(input);
-
+}
 
 
 //BEGIN popup global
@@ -150,6 +153,51 @@ if(jQuery('.double-arrows--wrap').length) {
     $(arrow).scrollLeft(value);
   });
 }
+
+//insides-retailers
+//slider 
+if(jQuery('.cost-retailers__slider').length) {
+
+  $('.cost-retailers__slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    // prevArrow: '<i class="fa fa-angle-left feedback-slider-arrows arrow-left_mob"></i>',
+    prevArrow: '<svg class="svg-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"></path></svg>',
+    // nextArrow: '<i class="fa fa-angle-right feedback-slider-arrows arrow-right_mob"></i>',
+    nextArrow: '<svg class="svg-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M17.525 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L205.947 256 10.454 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L34.495 36.465c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg>',
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1366,
+        settings: {
+          slidesToShow: 5
+        }
+      }
+    ]
+  });
+}
+
 
 });
 
