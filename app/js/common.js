0ee74c18c496a8960ba94913dabc327fabdbd62b
Елена Портная <e.portnaya@largespace.ru>
2018-12-03 19:14:53 +0200

    768

--- app/js/common.js
+++ app/js/common.js
@@ -162,30 +162,27 @@ if(jQuery('.cost-retailers__slider').length) {
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
-    arrows: true,
-    // prevArrow: '<i class="fa fa-angle-left feedback-slider-arrows arrow-left_mob"></i>',
-    prevArrow: '<svg class="svg-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"></path></svg>',
-    // nextArrow: '<i class="fa fa-angle-right feedback-slider-arrows arrow-right_mob"></i>',
-    nextArrow: '<svg class="svg-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M17.525 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L205.947 256 10.454 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L34.495 36.465c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg>',
+    arrows: false,
     responsive: [
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          arrows: false,
         }
       },
       {
         breakpoint: 768,
         settings: {
-          arrows: false,
           slidesToShow: 2
         }
       },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 3,
+          centerMode: true,
+          centerPadding: '10%',
+          initialSlide: 2
         }
       },
       {
